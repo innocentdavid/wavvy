ab678b9fee177cde483230a38f1c2915e4ff3e83
innocentdavid <paulinnocent04@gmail.com>
2023-08-06 13:47:45 +0100

    asd

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Nav from '@/components/Nav'
-import Hero from '@/components/Hero'
-import Features from '@/components/Features'
-import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,14 +8,7 @@ export default function Home() {
     <main
       className={`max-w-[1600px] mx-auto ${inter.className}`}
     >
-      <div className="fixed bottom-0 right-0 p-3 flex items-center gap-3">
-        <Image src="/badge-dark.svg" alt="" width={161} height={29} />
-      </div>
 
-      <Nav />
-      <Hero />
-      <Features />
-      <Footer />
     </main>
   )
 }
